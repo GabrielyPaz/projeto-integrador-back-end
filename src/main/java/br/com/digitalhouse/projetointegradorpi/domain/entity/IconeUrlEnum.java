fe6997385ca7ea7f06aa5d3265a6655d5e1fc1f0
andrericao <andrericao@hotmail.com>
2023-11-08 20:30:23 -0300

    Adicionada classe Enum de exemplo

---
+++ src/main/java/br/com/digitalhouse/projetointegradorpi/domain/entity/IconeUrlEnum.java
@@ -0,0 +1,18 @@
+package br.com.digitalhouse.projetointegradorpi.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum IconeUrlEnum {
+    
+    URL1("Ar condicionado"),
+    URL2("Portas"),
+    URL3("Direção Hidráulica"),
+    URL4("Câmbio Automático"),
+    URL5("Câmbio Manual");
+    
+    private String nomeUrl;
+    
+}
